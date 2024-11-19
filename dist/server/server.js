89f9a2c192d8816e7fa2516ae4dc49c90aedf244
dianaagl <diapant95@gmail.com>
2024-11-19 13:21:22 +0100

    dist/server

--- dist/server/server.js
+++ dist/server/server.js
@@ -5,21 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 const express_1 = __importDefault(require("express"));
-const testHandler_1 = require("./testHandler");
-const http_proxy_1 = __importDefault(require("http-proxy"));
 const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+const meals_1 = require("./controllers/meals");
+const dishes_1 = require("./controllers/dishes");
+const products_1 = require("./controllers/products");
+const ingredients_1 = require("./controllers/ingredients");
 const port = 4000;
 const expressApp = (0, express_1.default)();
-const proxy = http_proxy_1.default.createProxyServer({
-    target: "http://localhost:5100", ws: true
-});
+expressApp.use((0, cors_1.default)({ origin: 'http://localhost:3000' }));
 expressApp.use((0, helmet_1.default)());
 expressApp.use(express_1.default.json());
-expressApp.post("/test", testHandler_1.testHandler);
-expressApp.use(express_1.default.static("static"));
-expressApp.use(express_1.default.static("node_modules/bootstrap/dist"));
-expressApp.use((req, resp) => proxy.web(req, resp));
+(0, meals_1.registerMealInsert)(expressApp);
+(0, dishes_1.registerDishController)(expressApp);
+(0, products_1.registerProductController)(expressApp);
+(0, ingredients_1.registerIngredientController)(expressApp);
 const server = (0, http_1.createServer)(expressApp);
-server.on('upgrade', (req, socket, head) => proxy.
-    ws(req, socket, head));
+// server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head));
 server.listen(port, () => console.log(`HTTP Server listening on port ${port}`));
