48df64bef6e90d94fd10c0aef92522324983536d
dianaagl <diapant95@gmail.com>
2025-05-20 16:50:07 +0200

    login calls to api

--- src/client/src/context/ApiContextProvider.tsx
+++ src/client/src/context/ApiContextProvider.tsx
@@ -1,6 +1,5 @@
 // src/context/ApiContext.tsx
 import React, { createContext, useState, ReactNode, useContext } from 'react';
-import axios from 'axios';
 
 import { createMeal, deleteMeal, getAllMeals, getMealById, updateMeal, updateMealPart } from '../apis/mealsApi';
 import { useUser } from './UserContextProvider';
@@ -9,6 +8,7 @@ import { createProduct, deleteProduct, getAllProducts, getProductById, updatePro
 import { createIngredient, deleteIngredient, getIngredients, updateIngredient } from '../apis/ingredientsApi';
 import { useNavigate } from 'react-router-dom';
 import routes from '../routes/routes';
+import { loginUser, registerUser } from '../apis/usersApi';
 
 interface AxiosError<T = any> extends Error {
   response?: {
@@ -114,6 +114,11 @@ export const useApi = () => {
       get: handleApiCall(getIngredients),
       update: handleApiCall(updateIngredient),
       delete: handleApiCall(deleteIngredient)
+    },
+    users: {
+      create: handleApiCall(registerUser),
+      login: handleApiCall(loginUser),
+      
     }
     // Add more entities similarly
   };
